d132f669010c334dbb27e606930b5afc9b711a86
JoelsonSRodrigues <96163577+Joelson-S-Rodrigues@users.noreply.github.com>
2024-07-14 01:11:29 -0300

    modal creation
    modal creation

--- hiddenNumbers.js
+++ hiddenNumbers.js
@@ -1,94 +1,161 @@
 let timeLeft = 30;
 let timerInterval;
 let audioElement;
+let remainingAttempts = 3;
+let hits = 0;
+let gameRunning = false; // variável para controlar se o jogo está em andamento
+let audio; // variável para armazenar o elemento de áudio de martelo
 
+// Inicialização do timer
 function startTimer() {
   audioElement = document.getElementById("time-up-audio");
   audioElement.volume = 0.3;
-  audioElement.play(); // toca o áudio ao mesmo tempo que o timer começa
+  audioElement.play();
+  gameRunning = true;
   timerInterval = setInterval(() => {
     timeLeft--;
-    document.getElementById('time-left').innerHTML = timeLeft + '';
-    localStorage.setItem('timeLeft', timeLeft); // armazena o estado do timer
+    document.getElementById('time-left').innerHTML = `${timeLeft}s`;
+    localStorage.setItem('timeLeft', timeLeft);
     if (timeLeft <= 0) {
       clearInterval(timerInterval);
-      audioElement.pause(); // para o áudio quando o timer chega a 0
-      audioElement.currentTime = 0; // volta o áudio para o início
-      alert('Time\'s up!');
+      audioElement.pause();
+      audioElement.currentTime = 0;
+      showModal("Seu tempo acabou", "path/to/time-up-image.jpg");
+      resetGame();
     }
-  }, 1000); // 1000ms = 1 second
+  }, 1000);
 }
 
+// Reset do timer
 function resetTimer() {
-  timeLeft = 30; // reseta o timer
-  localStorage.removeItem('timeLeft'); // remove o estado do timer
-  audioElement.pause(); // para o áudio
-  audioElement.currentTime = 0; // volta o áudio para o início
+  timeLeft = 30;
+  localStorage.removeItem('timeLeft');
+  audioElement.pause();
+  audioElement.currentTime = 0;
 }
 
-// Carrega o estado do timer do localStorage
-if (localStorage.getItem('timeLeft')) {
-  timeLeft = parseInt(localStorage.getItem('timeLeft'));
-}
-
-// Initialize the timer when the page loads
-window.onload = startTimer;
-
-// Reset the timer when the page is updated
-window.onbeforeunload = resetTimer;
-
-function exibirNumeros() {
-    // Selecionar os spans com os números
-    const spans = document.querySelectorAll('span[id^="numero"]');
-    const aside = document.querySelector('aside.numberList');
-    const takenPositions = []; // array to store the positions of the previously placed spans
-  
-    // Função para gerar uma posição aleatória
-    function getRandomPosition() {
-      const maxX = Math.min(window.innerWidth, 1000) + 100; // ajuste para evitar que os números fiquem muito próximos da borda
-      const maxY = Math.min(window.innerHeight, 500) + 100; // ajuste para evitar que os números fiquem muito próximos da borda
-      let x, y;
-      do {
-        x = Math.floor(Math.random() * maxX);
-        y = Math.floor(Math.random() * maxY);
-        let isValidPosition = true;
-        for (const pos of takenPositions) {
-          if (distance(pos, { x, y }) < 50) {
-            isValidPosition = false;
-            break;
-          }
-        }
-        if (isValidPosition) {
-          takenPositions.push({ x, y }); // add the new position to the taken positions array
-          return { x, y };
-        }
-      } while (true);
+// Diminui o número de tentativas
+function decreaseAttempts() {
+  if (gameRunning) {
+    remainingAttempts--;
+    document.getElementById('remaining-attempts').textContent = `${remainingAttempts} / 3`;
+    if (remainingAttempts === 0) {
+      showModal("", "assets/aviso-derrota.png");
+      resetGame();
     }
-  
-    // função para calcular a distância entre dois pontos
-    function distance(pos1, pos2) {
-      return Math.sqrt(Math.pow(pos2.x - pos1.x, 2) + Math.pow(pos2.y - pos1.y, 2));
-    }
-  
-    // Loop para posicionar os spans aleatoriamente
-    spans.forEach((span) => {
-      const position = getRandomPosition();
-      span.style.position = 'absolute';
-      span.style.top = `${position.y}px`;
-      span.style.left = `${position.x}px`;
-  
-      // Adicionar evento de click a cada span
-      span.addEventListener('click', () => {
-        audio.play();
-        span.style.opacity = 1; // set opacity to 1
-        aside.appendChild(span); // move the span to the aside element
-        span.style.position = 'relative'; // reset position to relative
-        span.style.top = '0px'; // reset top position
-        span.style.left = '0px'; // reset left position
-      });
-    });
   }
-  
-  document.addEventListener('DOMContentLoaded', exibirNumeros);
+}
 
-  
+// Aumenta o número de acertos
+function increaseHits() {
+  hits++;
+  document.getElementById('hits-').textContent = `${hits} / 11`;
+  if (hits === 11) {
+    showModal('','assets/ganhador.png');
+    resetGame();
+  }
+}
+
+// Reseta o jogo
+function resetGame() {
+  clearInterval(timerInterval);
+  resetTimer();
+  gameRunning = false;
+  remainingAttempts = 3;
+  hits = 0;
+  document.getElementById('remaining-attempts').textContent = `${remainingAttempts} / 3`;
+  document.getElementById('hits-').textContent = `0 / 11`;
+  exibirNumeros();
+}
+
+// Função para exibir modais
+function showModal(message, imageUrl) {
+  const modal = document.getElementById('game-modal');
+  const modalMessage = document.getElementById('modal-message');
+  const modalImage = document.getElementById('modal-image');
+  const closeModal = document.getElementById('close-modal');
+
+  modalMessage.textContent = message;
+  modalImage.src = imageUrl;
+  modal.style.display = 'block';
+
+  closeModal.onclick = () => {
+    modal.style.display = 'none';
+  };
+
+  window.onclick = (event) => {
+    if (event.target == modal) {
+      modal.style.display = 'none';
+    }
+  };
+}
+
+// Inicialização do jogo: carrega elementos e inicia o timer
+document.addEventListener('DOMContentLoaded', () => {
+  audio = new Audio('assets/martelo.mp3');
+  exibirNumeros();
+  startTimer();
+});
+
+// Função para exibir números na tela
+function exibirNumeros() {
+  const spans = document.querySelectorAll('span.numero');
+  const aside = document.querySelector('aside.numberList');
+  aside.innerHTML = '';
+
+  const takenPositions = [];
+
+  function getRandomPosition() {
+    const maxX = Math.min(window.innerWidth, 1000) + 100;
+    const maxY = Math.min(window.innerHeight, 500) + 100;
+    let x, y;
+    do {
+      x = Math.floor(Math.random() * maxX);
+      y = Math.floor(Math.random() * maxY);
+      let isValidPosition = true;
+      for (const pos of takenPositions) {
+        if (distance(pos, { x, y }) < 50) {
+          isValidPosition = false;
+          break;
+        }
+      }
+      if (isValidPosition) {
+        takenPositions.push({ x, y });
+        return { x, y };
+      }
+    } while (true);
+  }
+
+  function distance(pos1, pos2) {
+    return Math.sqrt(Math.pow(pos2.x - pos1.x, 2) + Math.pow(pos2.y - pos1.y, 2));
+  }
+
+  spans.forEach((span) => {
+    const position = getRandomPosition();
+    span.style.position = 'absolute';
+    span.style.top = `${position.y}px`;
+    span.style.left = `${position.x}px`;
+
+    span.addEventListener('click', () => {
+      if (gameRunning) {
+        audio.play();
+        span.style.opacity = 1;
+        span.style.color = '#ffffff';
+        aside.appendChild(span);
+        span.style.position = 'relative';
+        span.style.top = '0px';
+        span.style.left = '0px';
+        increaseHits();
+      }
+    });
+  });
+
+  document.body.addEventListener('click', (event) => {
+    if (gameRunning) {
+      const clickedElement = event.target;
+      if (clickedElement.tagName === 'SECTION') {
+        decreaseAttempts();
+      }
+    }
+  });
+};
